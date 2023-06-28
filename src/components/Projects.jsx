a7b99b48f6b7c66f7127e4831534630dd42b2da9
DavidDGamble <daviddgamble@gmail.com>
2023-06-28 09:00:17 -0700

    Add content and styling to Projects component.

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,53 +1,30 @@
 import React, { useState } from "react";
-import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import Collapse from "react-bootstrap/Collapse";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
+import Button from "react-bootstrap/Button";
+import GeTogether from "../images/Getogether_Screenshot.png"
 import "../styles/Projects.css";
 
 const Projects = () => {
-  const [curDiv, setCurDiv] = useState(null);
   const [openDiv1, setOpenDiv1] = useState(false);
   const [openDiv2, setOpenDiv2] = useState(false);
 
   const handleButtonClick = (divNumber) => {
-    if (!curDiv & curDiv !== divNumber) {
-      setCurDiv(divNumber);
-      switch (divNumber) {
-        case 1:
-          setOpenDiv1(!openDiv1);
-          setOpenDiv2(false);
-          break;
-        case 2:
-          setOpenDiv2(!openDiv2);
-          setOpenDiv1(false);
-          break;
-        default:
-          break;
-      }
-    } else if (curDiv === divNumber) {
-      setOpenDiv1(false);
-      setOpenDiv2(false);
-      setCurDiv(null);
-    } else {
-      setOpenDiv1(false);
-      setOpenDiv2(false);
-    }
-  };
-
-  const handleDiv1Exited = () => {
-    switch (curDiv) {
+    switch (divNumber) {
       case 1:
         setOpenDiv1(!openDiv1);
+        setOpenDiv2(false);
         break;
       case 2:
         setOpenDiv2(!openDiv2);
+        setOpenDiv1(false);
         break;
       default:
         break;
     }
-  }
+  };
 
   return (
     <div className="projects">
@@ -55,22 +32,26 @@ const Projects = () => {
       <Row>
         <Col>
           <div className="selections">
-            <p onClick={() => handleButtonClick(1)}>Button 1</p>
-            <p onClick={() => handleButtonClick(2)}>Button 2</p>
+            <p className="link" onClick={() => handleButtonClick(1)}>GeTogether</p>
+            <p className="link" onClick={() => handleButtonClick(2)}>Button 2</p>
           </div>
         </Col>
         <Col>
-          <Collapse in={openDiv1} onExited={handleDiv1Exited}>
+          <Collapse in={openDiv1}>
             <div>
-              <Card.Body style={{ width: "400px" }}>
-                Project 1 Anim pariatur cliche reprehenderit, enim eiusmod high
-                life accusamus terry richardson ad squid. Nihil anim keffiyeh
-                helvetica, craft beer labore wes anderson cred nesciunt sapiente
-                ea proident.
-              </Card.Body>
+              <Card>
+                <Card.Img variant="top" src={GeTogether} />
+                <Card.Body>
+                  <Card.Title>GeTogether</Card.Title>
+                  <Card.Text>
+                    GeTogether is a robust web application built with Remix and Typescript while utilizing Postgres as the database management system. The app facilitates the creation and management of potluck style events, allowing users to organize and coordinate gatherings seamlessly. In addition to its powerful event management capabilities, the app employs Socket.io, a real-time communication library, to enable live communication between users associated with the events, fostering interactive and engaging experiences. With its combination of Typescript, Postgres, and Socket.io, GeTogether provides a reliable and dynamic platform for users to plan and participate in potluck events.
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
             </div>
           </Collapse>
-          <Collapse in={openDiv2} onExited={handleDiv1Exited}>
+          <Collapse in={openDiv2}>
             <div>
               <Card.Body style={{ width: "400px" }}>
                 Project 2 Anim pariatur cliche reprehenderit, enim eiusmod high
